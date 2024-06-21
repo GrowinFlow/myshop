d2bcaf6fce4b2322cf307727556ad4ab848e3867
growinflow <growinflowofficial@gmail.com>
2024-06-21 18:53:31 +0500

    env add

--- src/admin/Pages/Dashbord.js
+++ src/admin/Pages/Dashbord.js
@@ -15,8 +15,8 @@ function Dashbord() {
       <div className="container mx-auto min-h-[75vh] px-4 h-auto flex flex-col  gap-4">
 
 
-        <WelcomeCard text="Welcome &nbsp;" data={useName + "!"} />
-
+        <WelcomeCard text="Welcome &nbsp;" data={useName + "!"+ process.env.REACT_APP_BASE_URL} />
+<b>{process.env.API_BASE_URL}</b>
         <MainStats />
         <SecStats />
 

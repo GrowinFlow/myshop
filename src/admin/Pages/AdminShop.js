b160c5aa8d94d02a18b2575007c7e268cc622028
growinflow <growinflowofficial@gmail.com>
2024-06-22 22:52:16 +0500

    update env

--- src/admin/Pages/AdminShop.js
+++ src/admin/Pages/AdminShop.js
@@ -1,32 +1,30 @@
 import React from 'react';
-import ReactDOMServer from 'react-dom/server'; // Import ReactDOMServer
+import ReactDOMServer from 'react-dom/server';
 import Footer from '../../Common/Layout/Footer';
 import GlassCard from '../../Common/Components/GlassCard';
 import WelcomeCard from './../Component/PreComonents/WelcomeCard';
-import Sm from '../../Common/Pages/Sm';
-import ManageProducts from './ManageProducts'
+import ManageProducts from './ManageProducts';
 
 function AdminShop() {
   const componentHTML = ReactDOMServer.renderToStaticMarkup(<ManageProducts />);
-  
+
   return (
-    <>
-      <div className="container mx-auto min-h-[75vh] px-4 h-auto flex flex-col gap-4">
-        <WelcomeCard text="Client Side View" />
+    <div className="container mx-auto min-h-[75vh] px-4 h-auto flex flex-col gap-4">
+      <WelcomeCard text="Client Side View" />
 
-        <GlassCard>
-          <div className="themeGlassBg rounded-2xl p-4 ">
-            <iframe
-              srcDoc={`<!DOCTYPE html><html><head><title>React Component</title></head><body>${componentHTML} </body></html>`}
-              frameborder="0"
-              className='w-full min-h-96'
-            ></iframe>
-          </div>
-        </GlassCard>
+      <GlassCard>
+        <div className="themeGlassBg rounded-2xl p-4">
+          <iframe
+            srcDoc={`<!DOCTYPE html><html><head><title>React Component</title></head><body>${componentHTML}</body></html>`}
+            frameborder="0"
+            title='k'
+            className='w-full min-h-96'
+          ></iframe>
+        </div>
+      </GlassCard>
 
-        <Footer />
-      </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 

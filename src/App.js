9e26fe9217b492447e4ec5f0866d06636384cb3d
growinflow <growinflowofficial@gmail.com>
2024-07-03 23:46:29 +0500

    g

--- src/App.js
+++ src/App.js
@@ -31,13 +31,14 @@ function App() {
           <Routes>
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
-            <Route path="*" element={<PrivateRoutes />} /> {/* Catch all route */}
+            <Route path="*" element={<PrivateRoutes />} /> 
           </Routes>
 <Footer />
         </Main>
     
     </div>
-    </>
+    </>   
+
   );
 }
 
